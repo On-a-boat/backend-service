5f5b855392915061082c354a3e30453c632303f0
John Kim <kakwak123@gmail.com>
2021-09-19 18:00:35 +1000

    added db to our backend server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const app = express();
@@ -6,9 +7,21 @@ const app = express();
 // Use express' inbuilt body parser
 app.use(express.json(), cors());
 
+const con = mysql.createConnection({
+  host: "on-a-boat.cluster-czjflueg9kom.ap-southeast-2.rds.amazonaws.com",
+  user: "admin",
+  password: "on-a-boat",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.end();
+});
+
 // App routes
-app.use("/filter", require("./routes/filter"));
-app.use("/statistics", require("./routes/statistics"));
+// app.use("/filter", require("./routes/filter"));
+// app.use("/statistics", require("./routes/statistics"));
 
 app.listen(port, () => {
   console.log(`The app is listening on port ${port}`);
