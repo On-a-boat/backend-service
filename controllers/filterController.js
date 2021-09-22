96db451fa7af8ac02aba603ede64a018154d33b0
biao <50074141+A-Aias@users.noreply.github.com>
2021-09-23 01:24:12 +0800

    Clean the backend

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -1,8 +1,10 @@
-const serachUsers =async (req, res) => {}
+const serachUsers =async (req, res) => {
+    
+}
 
 const getUsers =async (req, res) => {
     const{age, keywords} = req.body;
-    
+
 }
 
 const getSingleUser =async (req, res) => {}
--- routes/filter.js
+++ routes/filter.js
@@ -2,11 +2,14 @@ const express = require("express");
 const filterController = require("../controllers/filterController");
 const router = express.Router();
 
+// allow admin to search a list of users by inputing age, keywords 
 router.post("/", filterController.searchUsers);
 
+// get a list of users with keywords 
 router.get("/:keywords", filterController.getUsers);
 
-router.get("/:name", filterController.getUser);
+// get information for a specific user by id 
+router.get("/:name", filterController.getSingleUser);
 
 
 module.exports = router;
