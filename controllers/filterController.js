997e371ff4b4e1cc85a65a8d20f338ada622e6c3
biao <50074141+A-Aias@users.noreply.github.com>
2021-10-04 02:33:59 +0800

    Update backend
    Finish the filter functions, move them to controller/routes folder

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -1,11 +1,43 @@
-const serachUsers =async (req, res) => {
-    
+const con = require("../middleware/db")
+
+// Get all the user information
+const findAll = async (req, res) => {
+    const queryString = "SELECT * FROM User"
+    con.query(queryString, function (err, result, fields) {
+        if (err) res.send(err);
+        if (result) res.json(result);
+      });
 }
 
-const getUsers =async (req, res) => {
-    const{age, keywords} = req.body;
+// Get the information of the a user by userid 
+const findUser = async (req, res) => {
+    const id = req.query.userId;
+    const queryString = "SELECT * FROM User WHERE UserId = ?"
+    con.query(queryString, [id], function (err, result, fields) {
+      if (err) res.send(err);
+      if (result) res.json(result);
+    });
+  };
+  
 
-}
-
-const getSingleUser =async (req, res) => {}
-
+// Filtering a list of user by age, gender and keywords, send all the information 
+const findUserByKeyword = async (req, res) => {
+    // default value for key and gender in queryString
+    var keywordString = "";
+    if (req.query.keywords){
+      const array = req.query.keywords.split(',');
+      for (i = 0; i < array.length; i++){
+        keywordString += " AND Keywords LIKE '" + "%" + array[i] + "%'";
+      } 
+    }
+  
+    const queryString = "SELECT * FROM User WHERE Age < 999" + keywordString;
+  
+    con.query(queryString, function (err, result, fields) {
+      if (err) res.send(err);
+      if (result) res.json(result);
+    });
+  
+  };
+  
+module.exports = {findAll, findUser, findUserByKeyword};
---
+++ middleware/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+// consturct the database
+const con = mysql.createConnection({
+    host: "mysql-db-team-90.czjflueg9kom.ap-southeast-2.rds.amazonaws.com",
+    user: "admin",
+    password: "on-a-boat",
+    database: "Newhope",
+  });
+
+
+// open the connection
+con.connect(error => {
+    if (error) throw error;
+    console.log("Successfully connected to the database.");
+});
+  
+
+module.exports = con;
