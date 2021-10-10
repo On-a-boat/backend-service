6c5c59b0755c53f75a05606fd1de1b653b3e41e0
Yifeng928 <88825161+Yifeng928@users.noreply.github.com>
2021-10-10 19:28:05 +1100

    Temp Change on CORS

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -1,7 +1,7 @@
 const con = require("../middleware/db");
 
 const showAll = async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Origin", "*");
   const queryString =
     "SELECT UserId, firstName,lastName,age,gender,freq,email,Link FROM newUser";
   con.query(queryString, function (err, result, fields) {
@@ -12,7 +12,7 @@ const showAll = async (req, res) => {
 
 // Get all the user information
 const findAll = async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Origin", "*");
   const queryString = "SELECT * FROM newUser";
   con.query(queryString, function (err, result, fields) {
     if (err) res.send(err);
@@ -22,7 +22,7 @@ const findAll = async (req, res) => {
 
 // Get the information of the a user by userid
 const findUser = async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Origin", "*");
   const id = req.query.userId;
   const queryString = "SELECT * FROM newUser WHERE UserId = ?";
   con.query(queryString, [id], function (err, result, fields) {
@@ -34,7 +34,7 @@ const findUser = async (req, res) => {
 // Filtering a list of user by age, gender and keywords, send all the information
 const findUserByKeyword = async (req, res) => {
   // default value for key and gender in queryString
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Origin", "*");
   var keywordString = "";
   if (req.query.keywords) {
     const array = req.query.keywords.split(",");
