3beae8c0e340cd910d4f67dfc341c0548ce74da6
Yifeng928 <88825161+Yifeng928@users.noreply.github.com>
2021-10-11 21:50:18 +1100

    add open email and content for one email

--- controllers/statisticController.js
+++ controllers/statisticController.js
@@ -30,13 +30,32 @@ const allGender = async (req, res) => {
 
 // Get Age 
 const allAge = async (req, res) => {
-    const queryString = 
-    "SELECT SUM(IF(age < 20,1,0)) as 'Under 20', SUM(IF(age BETWEEN 20 and 29,1,0)) as '20 - 29', SUM(IF(age BETWEEN 30 and 39,1,0)) as '30 - 39', SUM(IF(age BETWEEN 40 and 49,1,0)) as '40 - 49', SUM(IF(age BETWEEN 50 and 59,1,0)) as '50 - 59', SUM(IF(age BETWEEN 60 and 69,1,0)) as '60 - 69', SUM(IF(age BETWEEN 70 and 79,1,0)) as '70 - 79', SUM(IF(age BETWEEN 80 and 89,1,0)) as '80 - 89' FROM newUser;";
-    con.query(queryString, function (err, result, fields) {
-      if (err) res.send(err);
-      if (result) res.json(result);
-    });
-  };
+  const queryString = 
+  "SELECT SUM(IF(age < 20,1,0)) as 'Under 20', SUM(IF(age BETWEEN 20 and 29,1,0)) as '20 - 29', SUM(IF(age BETWEEN 30 and 39,1,0)) as '30 - 39', SUM(IF(age BETWEEN 40 and 49,1,0)) as '40 - 49', SUM(IF(age BETWEEN 50 and 60,1,0)) as '50 - 60' FROM newUser;";
+  con.query(queryString, function (err, result, fields) {
+    if (err) res.send(err);
+    if (result) res.json(result);
+  });
+};
 
+// Get the OpenedEmail from one email
+const findEmail = async (req, res) => {
+  const id = req.query.emailId;
+  const queryString = "SELECT SUM(NumberOpened) FROM Email WHERE EmailId = ?";
+  con.query(queryString, [id], function (err, result, fields) {
+    if (err) res.send(err);
+    if (result) res.json(result);
+  });
+};
 
-module.exports = { allOpenEmail, countAllUser, allGender , allAge};
+// Get the contents from one email
+const findContent = async (req, res) => {
+  const id = req.query.emailId;
+  const queryString = "SELECT Contents FROM Email WHERE EmailId = ?";
+  con.query(queryString, [id], function (err, result, fields) {
+    if (err) res.send(err);
+    if (result) res.json(result);
+  });
+};
+
+module.exports = { allOpenEmail, countAllUser, allGender , allAge, findEmail, findContent};
--- routes/statistics.js
+++ routes/statistics.js
@@ -14,4 +14,10 @@ router.get("/allGender", statistics.allGender);
 // Get allAge
 router.get("/allAge", statistics.allAge);
 
+// find opened Email for one Email
+router.get("/email", statistics.findEmail);
+
+// find content for one Email
+router.get("/content", statistics.findContent);
+
 module.exports = router;
