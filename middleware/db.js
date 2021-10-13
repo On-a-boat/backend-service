988f7de2d081f6784a17fa7880a58e8a37f3e854
John Kim <kakwak123@gmail.com>
2021-10-13 13:29:27 +1100

    changed dateformat

--- middleware/db.js
+++ middleware/db.js
@@ -2,18 +2,17 @@ const mysql = require("mysql");
 
 // consturct the database
 const con = mysql.createConnection({
-    host: "mysql-db-team-90.czjflueg9kom.ap-southeast-2.rds.amazonaws.com",
-    user: "admin",
-    password: "on-a-boat",
-    database: "Newhope",
-  });
-
+  host: "mysql-db-team-90.czjflueg9kom.ap-southeast-2.rds.amazonaws.com",
+  user: "admin",
+  password: "on-a-boat",
+  dateStrings: true,
+  database: "Newhope",
+});
 
 // open the connection
-con.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
+con.connect((error) => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
 });
-  
 
 module.exports = con;
